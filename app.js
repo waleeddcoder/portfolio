37cfe352bf254e137a1935bd7688c5df39e2ac52
Shahzeb Waleed <135195892+waleeddcoder@users.noreply.github.com>
2023-08-28 09:39:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -101,3 +101,18 @@ var mySwiper = new Swiper(".swiper-container", {
         nextEl: ".swiper-button-next",
     },
 });
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
